22d0311f4a768852beb224657ea1477c8529c309
lizhiqiang <gseROFrcO8CD6n6w>
2020-04-26 10:40:31 +0800

    文字颜色

--- app/src/main/java/com/smile/zqlib/MainActivity.kt
+++ app/src/main/java/com/smile/zqlib/MainActivity.kt
@@ -2,11 +2,17 @@ package com.smile.zqlib
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        tv_sm.setOnClickListener {
+            tv_sm.isSelected = !tv_sm.isSelected
+        }
     }
 }
--- zqview/src/main/java/com/smile/zqview/SmTextView.kt
+++ zqview/src/main/java/com/smile/zqview/SmTextView.kt
@@ -1,6 +1,7 @@
 package com.smile.zqview
 
 import android.content.Context
+import android.content.res.ColorStateList
 import android.content.res.Resources
 import android.graphics.Color
 import android.graphics.drawable.GradientDrawable
@@ -20,9 +21,22 @@ class SmTextView : AppCompatTextView {
     private var gradient = false
     private var gradientOrientation = 0
     private var bgColorFrom = Color.TRANSPARENT
+    private var bgColorFromSel = Color.TRANSPARENT
     private var bgColorTo = Color.TRANSPARENT
+    private var bgColorToSel = Color.TRANSPARENT
 
-    private lateinit var stateListDrawable: StateListDrawable
+    //文本
+    private var txtColor = Color.BLACK
+    private var txtColorSel = Color.BLACK
+
+    private lateinit var bgStateListDrawable: StateListDrawable
+    private lateinit var textColorStateList: ColorStateList
+
+    private val STATE_ENABLE = intArrayOf(android.R.attr.state_enabled)
+    private val STATE_UN_ENABLE = intArrayOf(-android.R.attr.state_selected)
+
+    private val STATE_NORMAL = intArrayOf(-android.R.attr.state_selected)
+    private val STATE_SELECT = intArrayOf(android.R.attr.state_selected)
 
     constructor(context: Context?) : this(context, null)
     constructor(context: Context?, attrs: AttributeSet?) : this(context, attrs, 0)
@@ -35,25 +49,36 @@ class SmTextView : AppCompatTextView {
     }
 
     private fun initViews(attrs: AttributeSet?) {
+
+
+        textColorStateList = textColors
+        val colorForState = textColorStateList.getColorForState(STATE_ENABLE, txtColor)
+
         attrs?.let {
 
             val a = context.obtainStyledAttributes(attrs, R.styleable.SmTextView)
 
             autoRound = a.getBoolean(R.styleable.SmTextView_sm_autoRound, false)
             bgColor = a.getColor(R.styleable.SmTextView_sm_bgColor, Color.TRANSPARENT)
+            bgColorSel = a.getColor(R.styleable.SmTextView_sm_bgColor_sel, bgColor)
             roundRadius = a.getDimension(R.styleable.SmTextView_sm_roundRadius, 0F)
 
             //渐变
             gradient = a.getBoolean(R.styleable.SmTextView_sm_gradient, false)
             bgColorFrom = a.getColor(R.styleable.SmTextView_sm_bgColorFrom, Color.TRANSPARENT)
+            bgColorFromSel = a.getColor(R.styleable.SmTextView_sm_bgColorFromSel, bgColorFrom)
             bgColorTo = a.getColor(R.styleable.SmTextView_sm_bgColorTo, Color.TRANSPARENT)
+            bgColorToSel = a.getColor(R.styleable.SmTextView_sm_bgColorToSel, bgColorTo)
             gradientOrientation = a.getInt(R.styleable.SmTextView_sm_gradient_orientation, 0)
+
+            //文本color
+            txtColor = a.getColor(R.styleable.SmTextView_sm_textColor, colorForState)
+            txtColorSel = a.getColor(R.styleable.SmTextView_sm_textColorSel, txtColor)
 
             a.recycle()
         }
 
-
-        stateListDrawable = if (background is StateListDrawable) {
+        bgStateListDrawable = if (background is StateListDrawable) {
             background as StateListDrawable
         } else {
             StateListDrawable()
@@ -65,27 +90,43 @@ class SmTextView : AppCompatTextView {
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
         setBg()
+        setTxtColor()
+    }
+
+    private fun setTxtColor() {
+        textColorStateList =
+            ColorStateList(arrayOf(STATE_NORMAL, STATE_SELECT), intArrayOf(txtColor, txtColorSel))
+        setTextColor(textColorStateList)
     }
 
 
     private fun setBg() {
 
-        val bg = GradientDrawable()
+        val bgNormal = GradientDrawable()
+        val bgSelect = GradientDrawable()
 
         if (autoRound) {
-            bg.cornerRadius = max(measuredWidth, measuredHeight).toFloat() / 2F
+            bgNormal.cornerRadius = max(measuredWidth, measuredHeight).toFloat() / 2F
+            bgSelect.cornerRadius = max(measuredWidth, measuredHeight).toFloat() / 2F
         } else {
-            bg.cornerRadius = roundRadius
+            bgNormal.cornerRadius = roundRadius
+            bgSelect.cornerRadius = roundRadius
         }
 
         if (gradient) {
-            bg.colors = intArrayOf(bgColorFrom, bgColorTo)
-            bg.orientation = getOrientation(gradientOrientation)
+            bgNormal.colors = intArrayOf(bgColorFrom, bgColorTo)
+            bgNormal.orientation = getOrientation(gradientOrientation)
+            bgSelect.colors = intArrayOf(bgColorFromSel, bgColorToSel)
+            bgSelect.orientation = getOrientation(gradientOrientation)
         } else {
-            bg.setColor(bgColor)
+            bgNormal.setColor(bgColor)
+            bgSelect.setColor(bgColorSel)
         }
 
-        background = bg
+        bgStateListDrawable.addState(STATE_NORMAL, bgNormal)
+        bgStateListDrawable.addState(STATE_SELECT, bgSelect)
+
+        background = bgStateListDrawable
     }
 
     private fun getOrientation(o: Int): GradientDrawable.Orientation {
